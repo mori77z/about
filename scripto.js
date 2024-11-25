612a7513fd13f8507bfe6b5243cfefdc311d83f2
mori77z <moritzggauss@gmail.com>
2024-11-25 02:22:29 +0100

    Update scripto.js

--- scripto.js
+++ scripto.js
@@ -9,67 +9,52 @@ function goBack() {
 }
 
 const translations = {
-  en: `
-    <p>I work at the intersection of industrial design, web design, and digital media. My focus lies in creating work that is both thoughtful and grounded, blending aesthetic considerations with practical application. I draw inspiration from subcultural movements and the raw, experimental aesthetics of early internet culture, which influence both the form and narrative of my projects.</p>
-    <p>Currently, I am pursuing a Master’s degree in Design at HfG Offenbach. My work often revolves around research-driven processes and interdisciplinary collaborations. I enjoy exploring unconventional approaches to design and value the balance between concept and execution. Whether it’s developing visual identities, designing digital experiences, or working on physical products, I aim to craft pieces that feel relevant and functional.</p>
-    <p>Portfolio available on <a href="mailto:moritzgg99@gmail.com">request</a></p>
+  "en": `
+    <p>Moritz Gauss, born in Stuttgart and currently working in Offenbach am Main, operates in the fields of digital art, web and user interface/user experience design, as well as creating concepts for objects and products. His approaches draw inspiration from early internet aesthetics and urban trends. His work as a designer in the digital space makes it possible to create cooperative and interdisciplinary projects on the internet, accessible by anyone from anywhere at any time.</p>
+    <p>After completing his bachelor's degree in industrial design at the Ostbayerische Technische Hochschule Regensburg, he is currently pursuing a master's degree in design at the Hochschule für Gestaltung Offenbach. In 2023, Moritz had the opportunity to work for 10 months as an artist-in-residence at M26 in Regensburg.</p>
   `,
-  de: `
-    <p>Ich arbeite im Spannungsfeld von Industriedesign, Webdesign und digitalen Medien. Mein Fokus liegt darauf, Arbeiten zu schaffen, die durchdacht und fundiert sind und Ästhetik mit praktischer Anwendung verbinden. Subkulturelle Bewegungen und die experimentelle Ästhetik des frühen Internets prägen sowohl die Form als auch die Erzählweise meiner Projekte.</p>
-    <p>Derzeit studiere ich im Masterstudiengang Design an der HfG Offenbach. Meine Arbeit basiert oft auf forschungsgeleiteten Prozessen und interdisziplinären Kollaborationen. Ich experimentiere gerne mit unkonventionellen Ansätzen und schätze die Balance zwischen Konzept und Umsetzung. Ob bei der Entwicklung visueller Identitäten, digitaler Experiences oder physischer Produkte – mir ist wichtig, dass meine Arbeit relevant und funktional ist.</p>
-    <p>Portfolio auf <a href="mailto:moritzgg99@gmail.com">Anfrage</a></p>
+  "de": `
+    <p>Moritz Gauss, geboren in Stuttgart und aktuell tätig in Offenbach am Main, arbeitet im Bereich digitale Kunst, Web- und User Interface/ User Experience Design sowie der Erstellung von Konzepten für Objekte und Produkte. Die Ansätze ziehen Inspiration aus Early Internet Aesthetics und urbanen Trends. Seine Arbeit als Gestalter im digitalen Raum ermöglicht es, kooperative und interdisziplinäre Projekte im Internet für jeden von überall und jederzeit zugänglich zu machen.</p>
+    <p>Nach dem Bachelorabschluss in Industriedesign an der Ostbayerischen Technischen Hochschule Regensburg studiert er derzeit im Masterstudiengang Design an der Hochschule für Gestaltung Offenbach. 2023 hatte Moritz 10 Monate die Gelegenheit, als Artist in Residence im M26 in Regensburg zu arbeiten.</p>
   `,
-    it: `
-    <p>Lavoro all'incrocio tra design industriale, web design e media digitali. Il mio obiettivo è creare opere ponderate e funzionali, combinando considerazioni estetiche con applicazioni pratiche. Mi ispiro ai movimenti subculturali e all'estetica sperimentale dell'inizio di internet, che influenzano sia la forma che il contenuto dei miei progetti.</p>
-    <p>Attualmente sto seguendo un Master in Design presso HfG Offenbach. Il mio lavoro spesso ruota attorno a processi basati sulla ricerca e collaborazioni interdisciplinari. Mi piace esplorare approcci non convenzionali al design e valorizzo l'equilibrio tra concetto ed esecuzione. Che si tratti di sviluppare identità visive, progettare esperienze digitali o lavorare su prodotti fisici, il mio obiettivo è creare opere rilevanti e funzionali.</p>
-    <p>Portfolio disponibile su <a href="mailto:moritzgg99@gmail.com">richiesta</a></p>
+  "it": `
+    <p>Moritz Gauss, nato a Stoccarda e attualmente attivo a Offenbach am Main, lavora nel campo dell'arte digitale, del design web e dell'interfaccia/esperienza utente, nonché nella creazione di concetti per oggetti e prodotti. I suoi approcci si ispirano all'estetica di internet degli inizi e alle tendenze urbane. Il suo lavoro come designer nello spazio digitale consente di creare progetti cooperativi e interdisciplinari su internet, accessibili da chiunque, ovunque e in qualsiasi momento.</p>
+    <p>Dopo aver conseguito la laurea in design industriale presso l'Ostbayerische Technische Hochschule Regensburg, sta attualmente proseguendo il suo master in design presso la Hochschule für Gestaltung Offenbach. Nel 2023, Moritz ha avuto l'opportunità di lavorare per 10 mesi come artista in residenza presso M26 a Regensburg.</p>
   `,
-  es: `
-    <p>Trabajo en la intersección entre diseño industrial, diseño web y medios digitales. Mi enfoque se centra en crear trabajos reflexivos y prácticos, fusionando consideraciones estéticas con aplicaciones funcionales. Me inspiran los movimientos subculturales y las estéticas experimentales de los primeros días de internet, que influyen tanto en la forma como en el contenido de mis proyectos.</p>
-    <p>Actualmente estoy cursando un máster en diseño en la HfG Offenbach. Mi trabajo suele girar en torno a procesos impulsados por la investigación y colaboraciones interdisciplinarias. Disfruto explorando enfoques poco convencionales y valoro el equilibrio entre concepto y ejecución. Ya sea desarrollando identidades visuales, diseñando experiencias digitales o trabajando en productos físicos, mi objetivo es crear piezas que sean relevantes y funcionales.</p>
-    <p>Portafolio disponible bajo <a href="mailto:moritzgg99@gmail.com">solicitud</a></p>
+  "es": `
+    <p>Moritz Gauss, nacido en Stuttgart y actualmente trabajando en Offenbach am Main, trabaja en el campo del arte digital, el diseño web y de interfaz/experiencia de usuario, así como en la creación de conceptos para objetos y productos. Sus enfoques se inspiran en la estética de internet de los primeros tiempos y en las tendencias urbanas. Su trabajo como diseñador en el espacio digital hace posible crear proyectos cooperativos e interdisciplinarios en internet, accesibles por cualquier persona desde cualquier lugar y en cualquier momento.</p>
+    <p>Después de completar su licenciatura en diseño industrial en la Ostbayerische Technische Hochschule Regensburg, actualmente está cursando una maestría en diseño en la Hochschule für Gestaltung Offenbach. En 2023, Moritz tuvo la oportunidad de trabajar durante 10 meses como artista residente en M26 en Regensburg.</p>
   `,
-  fr: `
-    <p>Je travaille à l’intersection du design industriel, du design web et des médias numériques. Mon objectif est de créer des œuvres à la fois réfléchies et ancrées, combinant des considérations esthétiques avec des applications pratiques. Je m’inspire des mouvements sous-culturels et des esthétiques expérimentales du début de l’internet, qui influencent à la fois la forme et le récit de mes projets.</p>
-    <p>Actuellement, je poursuis un Master en Design à la HfG Offenbach. Mon travail tourne souvent autour de processus basés sur la recherche et de collaborations interdisciplinaires. J’aime explorer des approches non conventionnelles et j’accorde de la valeur à l’équilibre entre le concept et l’exécution. Qu’il s’agisse de développer des identités visuelles, de concevoir des expériences numériques ou de travailler sur des produits physiques, mon objectif est de créer des pièces pertinentes et fonctionnelles.</p>
-    <p>Portfolio disponible sur <a href="mailto:moritzgg99@gmail.com">demande</a></p>
+  "fr": `
+    <p>Moritz Gauss, né à Stuttgart et actuellement basé à Offenbach am Main, travaille dans les domaines de l'art numérique, du design web et de l'expérience/utilisation des interfaces, ainsi que dans la création de concepts pour des objets et des produits. Ses approches s'inspirent des premières esthétiques d'internet et des tendances urbaines. Son travail en tant que designer dans l'espace numérique permet de créer des projets coopératifs et interdisciplinaires sur internet, accessibles à tous, partout et à tout moment.</p>
+    <p>Après avoir obtenu son diplôme de bachelier en design industriel à l'Ostbayerische Technische Hochschule Regensburg, il poursuit actuellement son master en design à la Hochschule für Gestaltung Offenbach. En 2023, Moritz a eu l'opportunité de travailler pendant 10 mois en tant qu'artiste en résidence à M26 à Regensburg.</p>
   `,
-  kyr: `
-    <p>Я працюю на перетині промислового дизайну, веб-дизайну та цифрових медіа. Моя мета — створювати роботи, які поєднують естетику з практичним застосуванням, ґрунтуючись на дослідницьких процесах. Натхнення я черпаю у субкультурних рухах та експериментальній естетиці раннього інтернету, що впливають як на форму, так і на зміст моїх проектів.</p>
-    <p>Наразі я здобуваю ступінь магістра дизайну в HfG Offenbach. Моя робота часто пов’язана з міждисциплінарною співпрацею та експериментами. Я люблю досліджувати нестандартні підходи до дизайну та прагну балансу між концепцією та реалізацією. Моєю метою є створення дизайнів, які є доречними та функціональними.</p>
-    <p>Портфоліо доступне за <a href="mailto:moritzgg99@gmail.com">запитом</a></p>
+  "zh": `
+    <p>Moritz Gauss，出生于斯图加特，目前在美因河畔的奥芬巴赫工作，从事数字艺术、网页和用户界面/用户体验设计，以及物体和产品概念的创作。他的设计方法从早期互联网美学和城市趋势中汲取灵感。他作为数字领域设计师的工作使得可以在互联网上创建合作性和跨学科的项目，使任何人都能随时随地访问。</p>
+    <p>在完成奥斯特贝尔技术大学的工业设计学士学位后，他目前正在奥芬巴赫的设计大学攻读硕士学位。2023年，Moritz有机会在瑞根斯堡的M26担任艺术驻地工作长达10个月。</p>
   `,
-  zh: `
-    <p>我专注于工业设计、网页设计和数字媒体的交叉领域。我的目标是创建既深思熟虑又实用的作品，将美学与实际应用相结合。我从亚文化运动和早期互联网的实验美学中汲取灵感，这些都影响着我的项目的形式和叙事。</p>
-    <p>目前，我正在HfG Offenbach攻读设计硕士学位。我的作品通常围绕以研究为驱动的过程和跨学科合作展开。我喜欢探索非传统的方法，并重视概念与执行之间的平衡。无论是开发视觉识别系统、设计数字体验，还是制作实体产品，我的目标都是打造相关且实用的作品。</p>
-    <p>作品集可<a href="mailto:moritzgg99@gmail.com">索取</a></p>
+  "ar": `
+    <p>موريتس جاوس، وُلد في شتوتغارت ويعمل حالياً في أوفنباخ على نهر الماين، يعمل في مجالات الفن الرقمي، تصميم الويب وتجربة المستخدم/واجهة المستخدم، بالإضافة إلى إنشاء مفاهيم للأشياء والمنتجات. تستلهم منهجياته الإلهام من جماليات الإنترنت المبكر ومن الاتجاهات الحضرية. يتيح له عمله كمصمم في الفضاء الرقمي إنشاء مشاريع تعاونية ومتعددة التخصصات على الإنترنت، يمكن الوصول إليها من قبل أي شخص من أي مكان وفي أي وقت.</p>
+    <p>بعد أن أكمل دراسته في مجال تصميم الصناعات في جامعة أوستبايريش التقنية في ريغنسبورغ، يواصل حالياً دراسته في درجة الماجستير في التصميم في جامعة هوخشوله فير جيستالتونغ أوفنباخ. في عام 2023، أتيحت لموريتس الفرصة للعمل لمدة 10 أشهر كفنان مقيم في M26 في ريغنسبورغ.</p>
   `,
-  vi: `
-    <p>Tôi làm việc tại giao điểm của thiết kế công nghiệp, thiết kế web và phương tiện kỹ thuật số. Mục tiêu của tôi là tạo ra những tác phẩm vừa cân nhắc vừa thực tế, kết hợp các yếu tố thẩm mỹ với ứng dụng thực tiễn. Tôi lấy cảm hứng từ các phong trào văn hóa phụ và thẩm mỹ thử nghiệm của thời kỳ đầu internet, điều này ảnh hưởng đến cả hình thức và nội dung các dự án của tôi.</p>
-    <p>Hiện tại, tôi đang theo học Thạc sĩ Thiết kế tại HfG Offenbach. Công việc của tôi thường xoay quanh các quy trình dựa trên nghiên cứu và hợp tác liên ngành. Tôi thích khám phá các phương pháp thiết kế phi truyền thống và đánh giá cao sự cân bằng giữa ý tưởng và thực thi. Dù là phát triển nhận diện thương hiệu, thiết kế trải nghiệm kỹ thuật số hay làm việc trên sản phẩm vật lý, mục tiêu của tôi là tạo ra những tác phẩm phù hợp và hữu ích.</p>
-    <p>Danh mục đầu tư có sẵn theo <a href="mailto:moritzgg99@gmail.com">yêu cầu</a></p>
+  "ja": `
+    <p>モーリッツ・ガウスはシュトゥットガルトで生まれ、現在はオッフェンバッハ・アム・マインで活動しており、デジタルアート、ウェブデザイン、ユーザーインターフェイス/ユーザーエクスペリエンスデザイン、そしてオブジェクトや製品のコンセプト作成の分野で活動しています。彼のアプローチは、初期インターネットの美学や都市のトレンドからインスピレーションを得ています。デジタル領域でのデザイナーとしての仕事は、インターネット上で協力的で学際的なプロジェクトを作り、誰でもどこでもいつでもアクセスできるようにします。</p>
+    <p>オスバイエル技術大学で産業デザインの学士号を取得後、現在はオッフェンバッハのデザイン大学で修士号を取得しています。2023年には、モーリッツはレゲンスブルクのM26でアーティスト・イン・レジデンスとして10か月間活動する機会を得ました。</p>
   `,
-  ja: `
-    <p>私は、工業デザイン、ウェブデザイン、デジタルメディアの交差点で活動しています。私の目標は、審美的な考慮と実践的な応用を融合させた、思慮深く実用的な作品を作ることです。サブカルチャーの動きや初期インターネットの実験的な美学にインスパイアされ、それが私のプロジェクトの形状と物語に影響を与えています。</p>
-    <p>現在、私はHfG Offenbachでデザインの修士号を取得中です。私の作品はしばしば、研究主導のプロセスや学際的なコラボレーションを中心に展開しています。非伝統的なデザインアプローチを探求し、コンセプトと実行のバランスを重視しています。視覚的なアイデンティティの開発、デジタル体験のデザイン、または物理的な製品の作業において、私の目標は、関連性と機能性を備えた作品を作成することです。</p>
-    <p>ポートフォリオは<a href="mailto:moritzgg99@gmail.com">リクエスト</a>で入手可能です</p>
+  "uk": `
+    <p>Моріц Гаус, народився в Штутгарті, а зараз працює в офенбаху на річці Майні, займається цифровим мистецтвом, веб-дизайном та дизайном користувацьких інтерфейсів/досвідом, а також створенням концепцій для об'єктів та продуктів. Його підходи черпають натхнення з естетики раннього інтернету та міських трендів. Його робота як дизайнера в цифровому просторі дозволяє створювати кооперативні та міждисциплінарні проекти в Інтернеті, доступні для будь-кого з будь-якого місця і в будь-який час.</p>
+    <p>Після здобуття ступеня бакалавра з промислового дизайну в Остбайєрській технічній школі Регенсбурга, він наразі продовжує навчання на магістратурі з дизайну в Гоффенбахській вищій школі дизайну. У 2023 році Моріц мав можливість працювати 10 місяців як художник-резидент у M26 в Регенсбурзі.</p>
   `,
-  ro: `
-    <p>Lucrez la intersecția dintre design industrial, design web și media digitale. Scopul meu este să creez lucrări care sunt atât bine gândite, cât și practice, combinând considerente estetice cu aplicații funcționale. Mă inspir din mișcările subculturale și estetica experimentală a internetului timpuriu, care influențează atât forma, cât și povestea proiectelor mele.</p>
-    <p>În prezent, urmez un masterat în design la HfG Offenbach. Munca mea se concentrează adesea pe procese bazate pe cercetare și colaborări interdisciplinare. Îmi place să explorez abordări neconvenționale ale designului și apreciez echilibrul dintre concept și execuție. Indiferent dacă este vorba despre dezvoltarea identităților vizuale, proiectarea experiențelor digitale sau lucrul la produse fizice, scopul meu este să creez lucrări relevante și funcționale.</p>
-    <p>Portofoliu disponibil la <a href="mailto:moritzgg99@gmail.com">cerere</a></p>
+  "ro": `
+    <p>Moritz Gauss, născut în Stuttgart și activ în prezent la Offenbach am Main, lucrează în domeniul artei digitale, al designului web și al interfeței/utilizării experienței utilizatorului, precum și în crearea de concepte pentru obiecte și produse. Abordările sale sunt inspirate de estetica internetului timpuriu și de tendințele urbane. Munca sa ca designer în spațiul digital face posibilă crearea de proiecte cooperative și interdisciplinare pe internet, accesibile oricui, din orice loc și în orice moment.</p>
+    <p>După ce a obținut licența în design industrial la Ostbayerische Technische Hochschule Regensburg, în prezent studiază la master în design la Hochschule für Gestaltung Offenbach. În 2023, Moritz a avut oportunitatea de a lucra 10 luni ca artist în rezidență la M26 în Regensburg.</p>
   `,
-  cs: `
-    <p>Pracuji na pomezí průmyslového designu, webdesignu a digitálních médií. Mým cílem je vytvářet promyšlené a praktické práce, které spojují estetické hledisko s funkční aplikací. Inspiraci čerpám z subkulturních hnutí a experimentální estetiky raného internetu, což ovlivňuje formu i příběh mých projektů.</p>
-    <p>V současné době studuji magisterský program designu na HfG Offenbach. Moje práce se často zaměřuje na procesy založené na výzkumu a interdisciplinární spolupráci. Rád zkoumám nekonvenční přístupy k designu a oceňuji rovnováhu mezi konceptem a realizací. Ať už jde o vývoj vizuálních identit, návrh digitálních zážitků nebo práci na fyzických produktech, mým cílem je vytvářet relevantní a funkční díla.</p>
-    <p>Portfolio k dispozici na <a href="mailto:moritzgg99@gmail.com">vyžádání</a></p>
-  `,
-  ar: `
-    <p>أعمل في تقاطع التصميم الصناعي وتصميم الويب والوسائط الرقمية. هدفي هو إنشاء أعمال مدروسة وعملية تجمع بين الاعتبارات الجمالية والتطبيقات العملية. أستمد إلهامي من الحركات الثقافية الفرعية والجماليات التجريبية للإنترنت المبكر، مما يؤثر على شكل ومضمون مشاريعي.</p>
-    <p>حاليًا، أتابع درجة الماجستير في التصميم في HfG أوفنباخ. غالبًا ما تركز أعمالي على العمليات القائمة على البحث والتعاون متعدد التخصصات. أستمتع باستكشاف الأساليب غير التقليدية للتصميم وأقدر التوازن بين المفهوم والتنفيذ. سواء كان ذلك في تطوير الهويات البصرية، أو تصميم التجارب الرقمية، أو العمل على المنتجات المادية، فإن هدفي هو إنشاء أعمال ذات صلة وعملية.</p>
-    <p>المحفظة متوفرة عند <a href="mailto:moritzgg99@gmail.com">الطلب</a></p>
+  "cs": `
+    <p>Moritz Gauss, narozený ve Stuttgartu a v současnosti pracující v Offenbachu am Main, působí v oblastech digitálního umění, webového a uživatelského rozhraní/zkušenosti, stejně jako v vytváření konceptů pro objekty a produkty. Jeho přístupy čerpají inspiraci z estetických počátků internetu a městských trendů. Jeho práce jako designéra v digitálním prostoru umožňuje vytvářet kooperativní a interdisciplinární projekty na internetu, dostupné každému z jakéhokoli místa a kdykoli.</p>
+    <p>Po dokončení bakalářského studia v oboru průmyslový design na Ostbayerische Technische Hochschule Regensburg, v současnosti studuje magisterské studium designu na Hochschule für Gestaltung Offenbach. V roce 2023 měl Moritz příležitost pracovat 10 měsíců jako umělec v rezidenci v M26 v Regensburgu.</p>
   `
 };
+
 
 function setLanguage(lang) {
   if (translations[lang]) {
